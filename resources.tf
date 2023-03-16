df756da87c45d4a92f7c649caf34057cf95c92b2
OmarSarhan <OSarhan@gmail.com>
2023-03-16 00:43:47 +0000

    remove vars

--- resources.tf
+++ resources.tf
@@ -1,10 +1,10 @@
 resource "google_container_cluster" "primary" {
-  name                     = var.cluster_name
-  location                 = var.location
-  project                  = var.project_id
-  subnetwork               = var.subnetwork
-  initial_node_count       = 1
-  remove_default_node_pool = false
+  name     = var.cluster_name
+  location = var.location
+  # project                  = var.project_id
+  # subnetwork               = var.subnetwork
+  # initial_node_count       = 1
+  # remove_default_node_pool = false
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
