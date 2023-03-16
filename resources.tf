c5225d2c53e7d6d75de107ddd2653f2a057427be
OmarSarhan <OSarhan@gmail.com>
2023-03-16 00:44:29 +0000

    add node count

--- resources.tf
+++ resources.tf
@@ -3,7 +3,7 @@ resource "google_container_cluster" "primary" {
   location = var.location
   # project                  = var.project_id
   # subnetwork               = var.subnetwork
-  # initial_node_count       = 1
+  initial_node_count = 1
   # remove_default_node_pool = false
 }
 
